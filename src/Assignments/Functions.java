d24625cbf6728f01024e5eb7191472389e81bf33
Abigail Godwin <princessabigail133@gmail.com>
2023-09-17 21:13:45 -0700

    Functions

---
+++ src/Assignments/Functions.java
@@ -0,0 +1,61 @@
+package Assignments;
+
+import org.junit.jupiter.api.Test;
+
+public class Functions {
+    public static boolean evenAndOdd(int number) {
+        int odd = 0;
+        int even = 1;
+        if (number % 2 == 0){
+            return true;
+        }else
+        return false;
+    }
+
+//    public static boolean primeNumber() {
+//        int number = 0;
+//        int userInput =1;
+//        int count = 1;
+//        while(count<1){
+//
+//        }
+//    }
+
+    public static int subtract(int number, int secondNumber) {
+            int result = 0;
+        if (number < secondNumber){
+             result = secondNumber - number;
+             return result;
+        }
+        else {
+            result = number - secondNumber;
+         return result;
+
+        }
+    }
+
+    public static double divide(int firstNumber,int secondNumber) {
+        double result =(double) firstNumber / secondNumber;
+        System.out.printf("%.2f",result);
+        return result;
+    }
+
+
+    public static int factor(int number) {
+        int count = 0;
+        int check = 0;
+        for (int counter =1; counter<= number;counter++){
+            check = number % counter;
+            if (check == 0){
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+    public static int square(int number) {
+
+
+    }
+}
