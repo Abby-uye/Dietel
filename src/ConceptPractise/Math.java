717b21341756634f4c999651b5a590bb33664446
Abigail Godwin <princessabigail133@gmail.com>
2023-09-20 13:16:46 -0700

    i added new files

---
+++ src/ConceptPractise/Math.java
@@ -0,0 +1,14 @@
+package ConceptPractise;
+
+import java.util.Scanner;
+
+public class Math {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter three numbers to find the largest");
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+        int number3 = input.nextInt();
+        System.out.printf("math.max %d %d %d ",number1,number2,number3);
+    }
+}
