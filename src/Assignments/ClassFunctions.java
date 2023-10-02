a87f045dcc1330773ecb2acc48b2e65267c3f71c
Jonathan Martins <85448311+dejmartins@users.noreply.github.com>
2023-10-03 01:01:09 +0300

    Reverse an array

--- src/Assignments/ClassFunctions.java
+++ src/Assignments/ClassFunctions.java
@@ -4,7 +4,13 @@ import java.util.Arrays;
 import java.util.function.Supplier;
 
 public class ClassFunctions {
-    public static int number(int[] numbers) {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] result = oddPosition(numbers);
+        System.out.println(Arrays.toString(result));
+    }
+    
+    public static int largestNumber(int[] numbers) {
         int largest = 0;
         for (int item = 0; item < numbers.length; item++) {
             if (numbers[item] > largest) {
@@ -13,6 +19,21 @@ public class ClassFunctions {
         }
         return largest;
 
+    }
+
+    public static int[] reverse(int[] numbers) {
+        // 1.
+        // Create a loop that loops through the array n/2 times
+        // For x element, swap it with n - x element (e.g swap 0th index with last index)
+            // Create a temp variable
+            // Assign index x value to temp
+            // Assign index n-x value to index x
+            // Assign temp to the index n - x
+
+        // 2.
+        // Create a new array of the same size as numbers array
+        // Loop through starting from index n-1 (the end)
+            // Assing each value to the new array from the start
     }
 
     public static boolean check(int[] numbers, int checker) {
@@ -33,12 +54,6 @@ public class ClassFunctions {
             count++;
         }
         return oddPosition;
-    }
-
-    public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] result = oddPosition(numbers);
-        System.out.println(Arrays.toString(result));
     }
 
     public static int runningTotal(int[] numbers) {
