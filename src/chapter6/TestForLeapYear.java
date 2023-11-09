ac4c69515baa5d29da5948981d3ff30d16894858
Abigail Godwin <princessabigail133@gmail.com>
2023-11-09 15:23:34 -0800

    I added leap year

---
+++ src/chapter6/TestForLeapYear.java
@@ -0,0 +1,30 @@
+package chapter6;
+
+public class TestForLeapYear {
+
+    private int year;
+    String yearType="";
+    public TestForLeapYear(int year){
+        if(year %4 == 0 || year%400 ==0){
+            yearType = "leap year";
+        }
+        this.year = year;
+    }
+    public void setYear(int year) {
+        if(year %4 == 0 || year%400 ==0){
+            yearType = "leap year";
+        }
+        this.year = year;
+    }
+    public String getYearType() {
+        return yearType;
+    }
+
+    public static void main(String[] args) {
+        TestForLeapYear leapYear = new TestForLeapYear(2016);
+        System.out.println(leapYear.getYearType());
+
+    }
+
+
+}
